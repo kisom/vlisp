c438c4feb1b11b76d48148039e991516b6f8bdc9
Kyle <kyle@tyrfingr.is>
2014-05-02 03:02:49 -0600

    Stub some definitions.
    This gives me immediate problems to think about.

--- Lisp.v
+++ Lisp.v
@@ -1,14 +1,75 @@
 (** * VLisp: a verified Lisp
 
 This is a Coq source file, documented and annotated using
-<<coqdoc>>. It aims to document the semantics and syntax of a
+_coqdoc_. It aims to document the semantics and syntax of a
 formally-verified Lisp, built from McCarthy's Equations of
 Software. 
 
+The Lisp 1.5 manual distinguishes between two types of
+expressions._S-expressions_, which are the symbolic expressions that
+comprise Lisp programs. _M-expressions_ are the _source language which
+specifies in what way the S-expressions are to be processed_. It is
+the job of the interpreter to convert S-expressions into
+M-expressions; this program will attempt to codify a representation of
+M-expressions with some representation of S-expressions.
+
 *)
 
-Inductive Truth : Set :=
-  | Nil : Truth
-  | T : Truth.
+(** ** Symbolic Expressions
 
-(* Extraction "Lisp.ml" *)
+Atoms are the basic building blocks of Lisp; the Lisp 1.5 manual notes
+the distinction between _atomic symbols_:
+
+<<
+Definition: an atomic symbol is a string of no more than thirty
+numerals and capital letters; the first character must be a letter.
+>>
+
+and _atom_, the predicate:
+
+<<
+atom[EXTRALONGSTRINGOFLETTERS]=T
+atom[(U . V)]=F
+atom[car[(U . V)]]=T
+>>
+
+*)
+
+Inductive atomic_symbol : Set :=  .
+
+(** A cell (as represented by an S-expression) contains of a first and second element; it is essentially a tuple (x, y) where x and y may be either an atomic symbol or a cell.*)
+
+Inductive cell : Set := NIL.
+
+(** ** Elementary Functions
+
+_cons_ is used to build S-expressions from atomic symbols and other S-expressions.
+
+<<
+cons[A;B]=(A . B)
+cons[(A . B);C]=((A . B) . C)
+cons[cons[A;B];C]=((A . B) . C)
+>>
+*)
+
+Definition cons (a b : cell) : cell := NIL. 
+
+(** _car_ and _cdr_ do the opposite of _cons_: they extract elements
+from an S-expression.
+
+<<
+The function car has one argument. Its value is the first part of
+its composite argument. car of an atomic symbol is undefined.
+>>
+*)
+
+Definition car (a : cell) : cell := NIL.
+
+(**
+<<
+The function cdr has one argument. Its value is the second part of its
+composite argument. cdr is also undefined if its argument is atomic.
+>>
+*)
+
+Definition cdr (a : cell) : cell := NIL.
