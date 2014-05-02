b7f6ab697ace5d47d7e6fadab7587ca67fa597dd
Kyle <kyle@tyrfingr.is>
2014-05-02 01:13:56 -0600

    Updating paper.

--- Lisp.v
+++ Lisp.v
@@ -1,5 +1,14 @@
-(** * VLisp: a verified Lisp *)
+(** * VLisp: a verified Lisp
+
+This is a Coq source file, documented and annotated using
+<<coqdoc>>. It aims to document the semantics and syntax of a
+formally-verified Lisp, built from McCarthy's Equations of
+Software. 
+
+*)
 
 Inductive Truth : Set :=
   | Nil : Truth
   | T : Truth.
+
+(* Extraction "Lisp.ml" *)
